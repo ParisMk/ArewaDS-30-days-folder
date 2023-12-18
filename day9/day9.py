5a0f109ffe282f5f2ab1edac82f37a5cb0e15e00
ParisMk <salmanmuhammad2020@gmail.com>
2023-12-18 10:00:05 -0800

    day9.py ArewaDS-30-days-folder

---
+++ day9/day9.py
@@ -0,0 +1,70 @@
+# Exercise level 1
+age = 30
+if age > 18:
+print('age You are old enough to learn to drive')
+age = 15
+if age < 18:
+print('age you need 3 more years to learn to drive')
+
+# compare the values of age
+my_age = 30
+if my_age < 35:
+    print('my_age is below yours')
+else:
+    print('my_age is equal to yours')
+
+# If greater
+    def maximum(a, b):
+if a >= b:
+    return a 
+else:
+    return b
+
+a = 2
+b = 4
+print(maximum(a, b))
+
+
+### Exercise: Level 2
+# Students grade
+
+if avg_marks >= 70:
+    print('Grade: A')
+elif avg_marks >= 60:
+    print('Grade: B')
+elif avg_marks >= 50:
+    print('Grade: C')
+elif avg_marks >= 45:
+    print('Grade: D')
+elif avg_marks >= 40:
+    print('Grade: E')
+else:
+    print('Grade: F')
+
+# Check season
+season = 'Winter'
+print(season)
+
+# modification
+fruits = ['banana','orange', 'mango', 'lemon',]
+print(fruits)
+new_fruit = fruits('apple')
+fruits.append(apple)
+print(fruits)
+
+person={
+'first_name':'Asabeneh',
+'last_name':
+'age': 250,
+'country':'Finland'
+'is_married': True,
+'skills': ['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],
+'address': {
+    'street': 'Space street',
+    'zipcode': '02210'
+}}
+
+print(person['skills'][2])
+print(person['skills'][4])
+print(person['skills']['He is a backend developer'])
+print(person['first_name']['last_name']['country']['is_married'])
