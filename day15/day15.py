bb539125f7f110f3781010bd337f667d596186fb
ParisMk <salmanmuhammad2020@gmail.com>
2023-12-22 11:27:20 -0800

    day15.py ArewaDS-30-days-folder

---
+++ day15/day15.py
@@ -0,0 +1,12 @@
+# Open your python interactive shell and try all the examples covered in this section
+print "HWLLSLA"
+# SyntaxError
+# NameError
+# IndexError: index out of range
+# ModuleNotFoundError
+# AttributeError
+# KeyError
+# TypeError
+# ImportError
+# ValueError
+# ZeroDivisionError
