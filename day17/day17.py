4d3924fa74539e84c65f9c23daa52ffa35f6a962
ParisMk <salmanmuhammad2020@gmail.com>
2023-12-23 03:32:24 -0800

    day17.py ArewaDS-30-days-folder

---
+++ day17/day17.py
@@ -0,0 +1,7 @@
+# Day 17, Exception handling
+
+names = ['Finland', 'Sweden', 'Norway', 'Denmark', 'Iceland', 'Estonia', 'Russia']
+nordic_countries, es, ru=names
+print(nordic_countries)
+print(es)
+print(ru)
